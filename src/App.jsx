ca6259d51f4ee7b00ee4c47ef178843419a8873d
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-02-21 22:55:30 +0100

    Deriving state & creating the logic in the final app

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ function App() {
         duration: 10,
     });
 
+    const inputIsValid = userInput.duration >= 1;
+
     function handleChange(inputIdentifier, newValue) {
         setUserInput((prevUserInput) => {
             return {
                 ...prevUserInput,
-                [inputIdentifier]: newValue,
+                [inputIdentifier]: +newValue,
             };
         });
     }
@@ -24,7 +26,12 @@ function App() {
         <>
             <Header />
             <UserInput userInput={userInput} onChange={handleChange} />
-            <Results userInput={userInput} />
+            {!inputIsValid && (
+                <p className="center">
+                    Please enter a duration greater than zero.
+                </p>
+            )}
+            {inputIsValid && <Results userInput={userInput} />}
         </>
     );
 }
